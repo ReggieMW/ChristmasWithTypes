1e85482b725408ac43ce7d415f89428f3d5bad32
ReggieMW <reggiewoods@msn.com>
2025-02-06 16:05:22 -0600

    Made requested changes including adding present list.

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -11,24 +11,24 @@ namespace ChristmasWithTypes
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            string Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            Array Presents =  new string[3]{ "PS5", "Nintendo Switch", "Puppy" }; 
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            int TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
-            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
+            Console.WriteLine($"Our tree will be {TreeHeight} feet high \n");
             Console.WriteLine("Here are the presents we would like:");
 
-            foreach (var present in xmas.Presents)
+            foreach (var present in Presents)
             {
                 Console.WriteLine($"    {present}");
             }
 
-            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+            Console.WriteLine($"\n We like to call Santa, {Santa}");
         }
     }
 }
